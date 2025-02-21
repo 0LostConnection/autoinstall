afc16f31de416103661554badd51c06073981122
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2025-02-21 13:35:02 -0300

    Add files via upload

---
+++ postinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb' -o /tmp/chrome_latest.deb
+dpkg -i /tmp/google_chrome.deb
+
+wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -o /tmp/code_latest.deb
+dpkg -i /tmp/code_latest.deb
